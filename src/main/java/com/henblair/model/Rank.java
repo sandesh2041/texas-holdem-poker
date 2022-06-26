6c8a97ceccdb1eb725b44110bf6abec833db52a0
bhendrickson <henblair@amazon.com>
2022-06-25 20:55:08 -0400

    Removed "blairsClass", added model for playing cards

---
+++ src/main/java/com/henblair/model/Rank.java
@@ -0,0 +1,39 @@
+package com.henblair.model;
+
+/**
+ * Enumerates all ranks of the standard 52-card deck of playing cards (aka French-suited playing cards), each with a
+ * short {@link String} abbreviation. No type of score or point value is included here, to enable incorporation into a
+ * wide variety of applications.
+ */
+public enum Rank {
+
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+
+    private final String abbreviation;
+
+    Rank(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    /**
+     * Returns the one- or two-letter abbreviation of this instance.
+     *
+     * @return
+     */
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+}
