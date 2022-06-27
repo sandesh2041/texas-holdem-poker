a1dd6d3bd5a6e832d6e88c39dcd5664862994aad
Sam <sandeshmas@gmail.com>
2022-06-27 13:57:34 -0500

    Added Multi card Action and bet to BotService

--- src/main/java/com/services/BotServices.java
+++ src/main/java/com/services/BotServices.java
@@ -25,4 +25,35 @@ public class BotServices {
         }
         return botAction;
     }
+
+    public Actions botMultiCardAction (Actions userAction, int score) {
+        Actions botAction = Actions.CALL;
+        if(userAction == Actions.CALL) {
+            if(score < 20){
+                botAction = Actions.FOLD;
+            }
+        } else if(userAction == Actions.CHECK) {
+            if(score < 20){
+                botAction = Actions.CHECK;
+            }
+        } else if(userAction == Actions.RAISE) {
+            if(score <= 20){
+                botAction = Actions.FOLD;
+            }
+        } else if(userAction == Actions.ALL_IN) {
+            if(score <= 30){
+                botAction = Actions.FOLD;
+            }
+        }
+        return botAction;
+
+    }
+
+    public int botBet(Actions botAction, int userBet) {
+        int bet = 10;
+        if(botAction == Actions.CALL) {
+            bet = userBet;
+        }
+        return bet;
+    }
 }
