0e7f9faedf752937245e05b55fdec0e34ec91286
bhendrickson <henblair@amazon.com>
2022-06-27 15:40:22 -0400

    Added Client class. Updated Game class for startGame/dealHands.

---
+++ src/main/java/com/game/Client.java
@@ -0,0 +1,18 @@
+package com.game;
+
+public class Client {
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        game.startGame();
+
+//        dealer.showHand();
+
+
+//        System.out.println("Would you like to play? 10$ minimum.");
+//        game.bet();
+//
+//        System.out.println("Would you like to Bet X2");
+    }
+}
