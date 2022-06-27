c73bb90630c29d86b0d8d0d42b1b021fe2189eeb
bhendrickson <henblair@amazon.com>
2022-06-27 15:39:34 -0400

    Added Client class. Updated Game class for startGame/dealHands.

--- src/main/java/com/game/Game.java
+++ src/main/java/com/game/Game.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
 public class Game {
 
     public static int bank = 0;
+    public static int pot = 0;
+    public static int blinds = 0;
     Deck deck = new Deck();
 
     private final Comparator<Card> displayComparator = Comparator
@@ -17,55 +19,50 @@ public class Game {
             .thenComparing(Card::getSuit)
             .thenComparing(Card::getRank);
 
-    //
-//    public static ArrayList<Card> user = new ArrayList<>();
-//    public static ArrayList<Card> table = new ArrayList<>();
-//    public static ArrayList<Card> dealer = new ArrayList<>();
-//        System.out.println(user.toString());
-//        System.out.println(table.subList(0,3));
-//        System.out.println(dealer.toString());
-    private ArrayList<Card> hand = new ArrayList<>();
+    private final ArrayList<Card> hand = new ArrayList<>();
+    private final ArrayList<Card> dealerHand = new ArrayList<>();
+    private final ArrayList<Card> burnPile = new ArrayList<>();
 
     public Game() {
     }
 
-    public static void main(String[] args) {
-
-        Game game = new Game();
-
-        game.startGame();
-
-//        dealer.showHand();
-
-
-//        System.out.println("Would you like to play? 10$ minimum.");
-//        game.bet();
-//
-//        System.out.println("Would you like to Bet X2");
-    }
-
     public void startGame() {
         Scanner scanner = new Scanner(System.in);
-        int addToBank;
         String startGame;
         String readyPlay;
 
         System.out.println("Welcome to Texas Hold'em! Select \"y\" or \"yes\" to play. ");
         startGame = scanner.next();
         if (startGame.equalsIgnoreCase("yes") || startGame.equalsIgnoreCase("y")) {
-            System.out.println("How much would you like to start with? enter a value between 10-1000: ");
+            System.out.println("How much would you like to start with? enter a value between 20-1000: ");
         } else {
             System.out.println("Exiting the game");
         }
 
-        while (scanner.nextInt() < 10 || scanner.nextInt() > 1000) {
-            System.out.println("You must select a whole value number between 10 and 1000!");
-                scanner.nextInt();
-
+        while (!(bank >= 20 && bank <= 1000)) {
+            try {
+                bank = Integer.parseInt(String.valueOf(Integer.parseInt(scanner.next())));
+            } catch (NumberFormatException ignored) {
+                System.out.println("You must select a whole value number between 20 and 1000!");
+            }
         }
-        addToBank = scanner.nextInt();
-        bank = addToBank;
-        System.out.println("Your current bank value is " + bank);
+        System.out.println("Your current bank value is $" + bank);
+        System.out.println("Please enter a value between 5-20 to use as the blind amount for each player");
+
+
+        while (blinds < 5 || blinds > 20) {
+            try {
+                blinds = Integer.parseInt(String.valueOf(Integer.parseInt(scanner.next())));
+            } catch (NumberFormatException ignored) {
+                System.out.println("You must select a whole value number between 5 and 20!");
+            }
+        }
+        System.out.println("Blind value set to $" + blinds);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
 
         System.out.println("Shuffling cards...");
@@ -93,11 +90,32 @@ public class Game {
         deck.shuffle();
         Card card1 = deck.draw();
         Card card2 = deck.draw();
-        hand.add(card1);
+        Card card3 = deck.draw();
+        Card card4 = deck.draw();
+        Card card5 = deck.draw();
+
+        burnPile.add(card1);
         hand.add(card2);
+        dealerHand.add(card3);
+        hand.add(card4);
+        dealerHand.add(card5);
+
+        System.out.println("Burn card has been dealt into the burn pile...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("Your first card is " + card1);
         try {
             Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Dealer has received their first card.");
+        try {
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -107,7 +125,15 @@ public class Game {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("Dealer has received their second card.");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         showHand();
+        takeBet();
 
 
     }
@@ -118,6 +144,10 @@ public class Game {
     }
 
     public void takeBet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("$");
+        System.out.println("It is your turn, please enter \"c\" or \"check\" to check, or y");
+        String preFlop = scanner.next();
 
     }
 
