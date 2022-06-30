221637fe9c49679751db9b65f8e77d5a8a217e9d
Agustin <agustinduranse@gmail.com>
2022-06-30 14:01:19 -0700

    Rank Enum javadocs

--- src/main/java/com/model/team/Rank.java
+++ src/main/java/com/model/team/Rank.java
@@ -7,20 +7,62 @@ package com.model.team;
  */
 public enum Rank {
 
+    /**
+     * Enum Rank of Two.
+     */
     TWO("2"),
+    /**
+     * Enum Rank of Three.
+     */
     THREE("3"),
+    /**
+     * Enum Rank of Four.
+     */
     FOUR("4"),
+    /**
+     * Enum Rank of Five.
+     */
     FIVE("5"),
+    /**
+     * Enum Rank of Six.
+     */
     SIX("6"),
+    /**
+     * Enum Rank of Seven.
+     */
     SEVEN("7"),
+    /**
+     * Enum Rank of Eight.
+     */
     EIGHT("8"),
+    /**
+     * Enum Rank of Nine.
+     */
     NINE("9"),
+    /**
+     * Enum Rank of Ten.
+     */
     TEN("10"),
+    /**
+     * Enum Rank of Jack.
+     */
     JACK("J"),
+    /**
+     * Enum Rank of Queen.
+     */
     QUEEN("Q"),
+    /**
+     * Enum Rank of King.
+     */
     KING("K"),
+    /**
+     * Enum Rank of Ace.
+     */
     ACE("A");
 
+    /**
+     * Sets the abbreviation of the Ranks.
+     */
     private final String abbreviation;
 
     Rank(String abbreviation) {
@@ -30,7 +72,7 @@ public enum Rank {
     /**
      * Returns the one- or two-letter abbreviation of this instance.
      *
-     * @return
+     * @return The {@link Rank} of the Cards.
      */
     public String getAbbreviation() {
         return abbreviation;
